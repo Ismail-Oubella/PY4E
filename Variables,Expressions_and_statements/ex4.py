372b65e62cd2d0e9e572d83ee7ad15b317c7789b
Ismail-Oubella <oubellaismail00@gmail.com>
2023-05-23 13:11:25 +0100

    This is the solution of the first assignements of Python for everybody course.

---
+++ Variables,Expressions_and_statements/ex4.py
@@ -0,0 +1,7 @@
+width = 17
+height = 12.0
+
+print(width//2 )# this is // operator represents the integer division so result => 8
+print(width/2.0) # result => 8.5
+print(height/3 )# result => 4.0 
+print(1 + 2 * 5) #result => 11
---
+++ Variables,Expressions_and_statements/ex5.py
@@ -0,0 +1,3 @@
+celsius = input('Enter temperator in Celsius: ')
+fah = (float(celsius)*9/5) + 32
+print("Temperator in Fahrenheit is:", fah)
